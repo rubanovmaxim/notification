604511164e3c99f3fba1112ac69bb7836c28a3b3
rubanovmaxim <maksru@mail.ru>
2020-01-27 00:07:54 +0300

    Работа с очередями

--- src/main/java/ru/bookstore/Main.java
+++ src/main/java/ru/bookstore/Main.java
@@ -1,10 +1,18 @@
-package ru.otus.architect.notification;
+package ru.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Main {
+
+    @GetMapping
+    public String home(){
+        return "notification service";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
--- src/test/java/ru/bookstore/NotificationApplicationTests.java
+++ src/test/java/ru/bookstore/NotificationApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.otus.architect.notification;
+package ru.bookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
